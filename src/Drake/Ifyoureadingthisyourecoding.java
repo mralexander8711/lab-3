67b8d3e1fba205450066c2785bbd7efcd5b47373
mralexander8711 <mralexander8711@icloud.com>
2019-01-16 00:49:52 -0500

    lab 3

---
+++ src/Drake/Ifyoureadingthisyourecoding.java
@@ -0,0 +1,101 @@
+package Drake;
+import java.util.Scanner;
+
+public class Ifyoureadingthisyourecoding {
+
+	static Scanner scnr = new Scanner(System.in);
+	static Scanner scna = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String answer = "100";
+		String weak = "No";
+		
+		
+		System.out.println("Welcome Aubrey!");
+		// Ask Question?/
+		
+		System.out.println("Are we going fast today? Yes/No");
+		//Input Response//
+		String userInput = "Yes";
+		
+		userInput = scnr.next();
+		
+		if(userInput.equalsIgnoreCase("Yes"));{
+		System.out.println("How Fast?");
+		}
+		//choice = scnr.nextLine();
+		weak = sc.nextLine();
+		//Either continue or break
+		  
+		
+		//if(choice.equalsIgnoreCase("Yes");
+			//System.out.println("How Fast?");{			
+			//}
+		//Else(weak.equalsIgnoreCase("No"));
+			//System.out.println("Meek got to you huh? Ok Goodbye");
+			
+			
+       }		
+			
+  //Input UserSpeed			
+			
+			int userSpeed; {
+				userSpeed = scna.nextInt();{}
+				
+				
+			while (userSpeed < 1 || userSpeed > 100);
+
+			 if(userSpeed % 2 != 0 && userSpeed < 60) {
+				System.out.println(userSpeed + " Oddly enough thats not fast enough,Try again");
+			} else if (userSpeed % 2 == 0 && userSpeed < 25) {
+				System.out.println("Even Then That's Not Fast Enough,Try again ");
+			} else if (userSpeed % 2 == 0 && userSpeed <= 60) {
+				System.out.println("Even More Faster, Try Again ");
+			} 
+				else if (userSpeed % 2 == 0 && userSpeed > 60) {
+				System.out.println(userSpeed + " Even Now, Man neeed to go faster, Try Again ");
+				userSpeed = scna.nextInt();{}
+
+
+			}
+			
+			else if (userSpeed % 2 != 0 && userSpeed > 60) {
+				System.out.println(userSpeed + " Oddly You need go faster then 60mps,Try Again ");
+				userSpeed = scna.nextInt();
+
+			}
+			
+			
+			
+			else if(userSpeed==100); {
+					System.out.println("Thats the Drake I Know?");			
+			
+			
+			
+		}
+			
+		}
+
+		
+		private static void Else(boolean equalsIgnoreCase) {
+			// TODO Auto-generated method stub
+			
+		}		 }
+	
+		//Input User speed
+
+		 
+
+
+	
+		 
+	
+	
+		 
+                  
+    
+
+
